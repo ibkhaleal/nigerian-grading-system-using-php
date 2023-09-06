a5ea2c773bba5947599cd336b47552fe883ffaac
Ibrahim Hassan Muhammad <50172443+ibkhaleal@users.noreply.github.com>
2023-09-06 17:51:40 +0100

    updated the grading function to display only letters
    updated the grading function to display only letters

--- grading.php
+++ grading.php
@@ -1,14 +1,14 @@
 <?php function determineGrade($score) {
     if ($score >= 70 && $score <= 100) {
-        return "A (Excellent)";
+        return "A";
     } elseif ($score >= 60 && $score < 70) {
-        return "B (Very Good)";
+        return "B";
     } elseif ($score >= 50 && $score < 60) {
-        return "C (Good)";
+        return "C";
     } elseif ($score >= 45 && $score < 50) {
-        return "D (Pass)";
+        return "D";
     } elseif ($score >= 0 && $score < 45) {
-        return "F (Fail)";
+        return "F";
     } else {
         return "Invalid Score";
     }
